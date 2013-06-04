d6552b1eaad5fbacc9458d936b11aa7f8a215dfc
apismensky <alexey.pismenskiy@octanner.com>
2013-06-04 15:10:16 -0600

    reqiore 100% coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 require 'simplecov'
 SimpleCov.start
 
+SimpleCov.minimum_coverage 100
+
+SimpleCov.at_exit do
+  SimpleCov.result.format!
+  if SimpleCov.result.covered_percent < SimpleCov.minimum_coverage
+    fail_msg = "#{'%.2f' % SimpleCov.result.covered_percent}% test coverage?? Really??!!"
+    STDERR.puts "\033[0;31m#{fail_msg}\033[0m\nWrite more tests"
+    exit 1
+  end
+end
+
 require 'rack/test'
 require 'rack-auth-oc_tanner'
 require 'ostruct'
