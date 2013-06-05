fa4b81217f0cdb62c4128bd5abc4170fad105b4f
apismensky <alexey.pismenskiy@octanner.com>
2013-06-05 13:46:27 -0600

    change required coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,13 @@
 require 'simplecov'
 SimpleCov.start
 
-puts "Ruby Version: #{RUBY_VERSION}"
-
-SimpleCov.minimum_coverage 101
+SimpleCov.minimum_coverage 0
 
 SimpleCov.at_exit do
   SimpleCov.result.format!
   if SimpleCov.result.covered_percent < SimpleCov.minimum_coverage
     fail_msg = "#{'%.2f' % SimpleCov.result.covered_percent}% test coverage?? Really??!!"
     STDERR.puts "\033[0;31m#{fail_msg}\033[0m\nWrite more tests"
-    system("ls -lR")
     exit 1
   end
 end
