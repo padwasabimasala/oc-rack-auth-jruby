16349391d36bed7569f36050d43dbe754ef62a96
apismensky <alexey.pismenskiy@octanner.com>
2013-06-05 13:41:40 -0600

    Added coverage report debugging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,14 @@ SimpleCov.start
 
 puts "Ruby Version: #{RUBY_VERSION}"
 
-SimpleCov.minimum_coverage 100
+SimpleCov.minimum_coverage 101
 
 SimpleCov.at_exit do
   SimpleCov.result.format!
   if SimpleCov.result.covered_percent < SimpleCov.minimum_coverage
     fail_msg = "#{'%.2f' % SimpleCov.result.covered_percent}% test coverage?? Really??!!"
     STDERR.puts "\033[0;31m#{fail_msg}\033[0m\nWrite more tests"
+    system("ls -lR")
     exit 1
   end
 end
