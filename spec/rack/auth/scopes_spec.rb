7af5e6d5cdd9d2fb0e85f26b91d3b0addd28e367
Jay Wagnon <jay.wagnon@gmail.com>
2013-09-16 09:07:53 -0600

    Added ordinal spec.

--- spec/rack/auth/scopes_spec.rb
+++ spec/rack/auth/scopes_spec.rb
@@ -15,6 +15,16 @@ describe Rack::Auth::Scopes do
       scope_map = scopes.instance_variable_get(:@scopes)
       scope_map.keys.join(', ').should eq scope_string
     end
+
+    it 'assigns zero-based ordinals to the scopes in the map' do
+      scopes = Rack::Auth::Scopes.new scope_string
+      scope_map = scopes.instance_variable_get(:@scopes)
+      ordinal = 0
+      scope_string.split(',').map(&:strip).each do |scope|
+        scope_map[scope].should eq ordinal
+        ordinal = ordinal + 1
+      end
+    end
   end
 
 end
